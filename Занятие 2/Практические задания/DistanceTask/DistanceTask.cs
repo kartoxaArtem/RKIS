5c868127df6526aed3a6b5453441fa269d41ef8e
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-18 17:50:38 +0300

    lesson 2: make DistanceTask

--- Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
+++ Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
@@ -1,12 +1,29 @@
 using System;
-
-namespace DistanceTask;
-
-public static class DistanceTask
+ 
+namespace DistanceTask
 {
-    // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
-    public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+    public static class DistanceTask
     {
-        return 0.0;
+        // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+        public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+        {
+            double a = Math.Sqrt(((x - ax) * (x - ax)) + ((y - ay) * (y - ay)));
+            double b = Math.Sqrt(((x - bx) * (x - bx)) + ((y - by) * (y - by)));
+            double c = Math.Sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)));
+ 
+            if (x >= ax && x <= bx && b != 0)
+            {
+ 
+                                double e = (a + b + c) / 2.0;
+                                double f = Math.Sqrt((e * (e - a) * (e - b) * (e - c)));
+ 
+                                return (2.0 * f) / c;
+                            }
+                    else if ((x <= ax || x >= bx) && c != 0)
+                    {
+                        return Math.Min(a, b);
+                    }
+                    else return 0;
+        }
     }
 }
