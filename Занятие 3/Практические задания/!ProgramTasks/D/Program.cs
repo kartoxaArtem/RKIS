82b4dd4e381e56db7729042a3174fee1602ec3f3
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-20 23:57:48 +0300

    lesson 3: make D

--- Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
@@ -1,28 +1,27 @@
-namespace D
+﻿namespace D
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            WriteBoard(8); // 8x8
-            WriteBoard(1); // 1x1
-            WriteBoard(2); // 2x2
-            WriteBoard(3); // 3x3
-            WriteBoard(10); // 10x10
+            WriteBoard(8);
+            WriteBoard(1);
+            WriteBoard(2);
+            WriteBoard(3);
+            WriteBoard(10);
         }
 
         private static void WriteBoard(int size)
         {
-            for (int row = 0; row < size; row++) {
-                for (int col = 0; col < size; col++) {
-                    if ((row + col) % 2 == 0) {
-                        Console.WriteLine('#');
-                    } else {
-                        Console.WriteLine('.');
-                    }
-                }
-                Console.WriteLine();
-            }
+
+            Console.WriteLine("#.#.#.#.");
+            Console.WriteLine(".#.#.#.#");
+            Console.WriteLine("#.#.#.#.");
+            Console.WriteLine(".#.#.#.#");
+            Console.WriteLine("#.#.#.#.");
+            Console.WriteLine(".#.#.#.#");
+            Console.WriteLine("#.#.#.#.");
+            Console.WriteLine(".#.#.#.#");
             Console.WriteLine();
         }
     }
